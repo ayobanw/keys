189e10a35b44f1da413fda05263dceb4ec2f649a
ANW®©™✓ <carpkingrc1@gmail.com>
2025-06-25 02:25:58 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,133 @@
 local url = "https://raw.githubusercontent.com/ayobanw/keys/refs/heads/main/keys.txt"
+
+if os.date("%Y%m%d") > "20251230" then
+time=gg.alert([[
+                     ☣️THIS SCRIPT HAS EXPIRED ☣️
+🔸PLEASE REFER TO THE DEVELOPER TO RENEW YOUR SCRIPT🔸
+       🔹 TELEGRAM: ayobcicaknakal  🔹TIKTOK: SB_KP
+
+                         ╭══•  ೋ•✧๑♡๑✧•ೋ  •══╮
+
+                            𝑺𝑪𝑹𝑰𝑷𝑻 𝑩𝒀 𝑨𝒀𝑶𝑩 𝑷𝑹𝑶𝑮𝑹𝑨𝑴
+                                    ☣️🅰🅽🆆 ®©™☣️                               
+
+                         ╰══•  ೋ•✧๑♡๑✧•ೋ  •══╯]])
+os.exit()
+end
+
+
+print("𝐒𝐄𝐁𝐀𝐑𝐀𝐍𝐆 𝐏𝐄𝐑𝐓𝐀𝐍𝐘𝐀𝐀𝐍 𝐓𝐄𝐍𝐓𝐀𝐍𝐆 𝐒𝐂𝐑𝐈𝐏𝐓 𝐁𝐎𝐋𝐄𝐇 𝐓𝐄𝐑𝐔𝐒 𝐏𝐌 𝐃𝐈 𝐓𝐈𝐊𝐓𝐎𝐊 𝐒𝐁_𝐊𝐏")
+
+gg.setVisible (false)
+gg.sleep(300)                             
+gg.sleep(300)
+gg.alert([[
+         ╭══•             ೋ•✧๑♡๑✧•ೋ.            •══╮
+         
+           𝑾𝑬𝑳𝑪𝑶𝑴 𝑻𝑶 𝑺𝑪𝑹𝑰𝑷𝑻 𝑩𝒀 𝑨𝒀𝑶𝑩 𝑷𝑹𝑶𝑮𝑹𝑨𝑴
+                                ☣️ 🅰🅽🆆 ®©™ ☣️
+                                          
+         ╰══•             ೋ•✧๑♡๑✧•ೋ.            •══╯]])
+  gg.sleep(300)
+  gg.toast("💫SABAR JAP")
+  gg.sleep(300)
+  gg.toast("🆃")
+  gg.sleep(500)
+  gg.toast("🆃🅸")
+  gg.sleep(500)
+  gg.toast("🆃🅸🅺")
+  gg.sleep(500)
+  gg.toast("🆃🅸🅺🆃")
+  gg.sleep(500)
+  gg.toast("🆃🅸🅺🆃🅾")
+  gg.sleep(500)
+  gg.toast("🆃🅸🅺🆃🅾🅺")
+  gg.sleep(500)
+  gg.toast("🆃🅸🅺🆃🅾🅺 🅰")
+  gg.sleep(500)
+  gg.toast("🆃🅸🅺🆃🅾🅺 🅰🅽")
+  gg.sleep(500)
+  gg.toast("🆃🅸🅺🆃🅾🅺 🅰🅽🆆")
+  gg.sleep(500)
+  gg.toast("🆃🅸🅺🆃🅾🅺 🅰🅽🆆®")
+  gg.sleep(500)
+  gg.toast("🆃🅸🅺🆃🅾🅺 🅰🅽🆆®©")
+  gg.sleep(500)
+  gg.toast("🆃🅸🅺🆃🅾🅺 🅰🅽🆆®©™")
+  gg.sleep(1000)
+  gg.toast("🅿🆁🅴🆂🆂 🅶🅶 🆃🅾 🅾🅿🅴🅽")
+  gg.setVisible(false)
+  gg.alert("JOM LAYAN LONGGOK 🤣😂")
+
+function start()
+menu = gg.choice({
+	" [ CONTEROL GLISH ]",
+	" [ TAYAR 💯 ]",
+	" [ HANDBRACK ]",
+	"                  NEW 𝐕𝐄𝐑𝐒𝐈𝐎𝐍 ",
+	"KELUAR🚪"},nil," SCRIPT MENU COIN\n\n BY : ☣️🅰🅽🆆 ®©™☣️")
+                                                                                                                                                                                                                                                                                                                                                                                       
+if menu == 1 then
+gg.setVisible(false)
+gg.clearResults()
+gg.clearList()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("9.18354962e-41;0.15000000596;0.25:17", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.refineNumber("0.15000000596", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.toast("Aktif")
+gg.processResume()
+end
+
+--tayar
+if menu == 2 then
+gg.clearResults()
+gg.clearList()
+-- main code on
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("-0.00005", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("9999999", gg.TYPE_FLOAT)
+gg.toast("ON")
+gg.processResume()
+end
+
+--hanbrek
+if menu == 3 then
+gg.clearResults()
+gg.clearList()
+-- main code on
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("6000", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("-4500", gg.TYPE_FLOAT)
+gg.toast("ON")
+gg.processResume()
+end
+
+if menu == 4 then
+gg.clearResults()
+gg.clearList()
+-- main code info
+gg.clearResults()
+end
+
+if menu ==  5 then
+gg.clearResults()
+gg.clearList()
+os.exit()
+end
+
+if menu == nil then end
+menuk = - 1
+end
+
+while(true) do
+if gg.isVisible(true) then
+menuk = 1
+gg.setVisible(false)
+end
+
+if menuk == 1 then start() end end
