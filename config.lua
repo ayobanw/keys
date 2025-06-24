6872bd04dc65619a1465a7db2164f1293a3d097c
ANW®©™✓ <carpkingrc1@gmail.com>
2025-06-25 04:59:15 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,2 +1,2 @@
 -- Config (boleh diubah jika repo atau URL berubah)
-REPO_URL = "https://raw.githubusercontent.com/ayobanw/keys.txt/main/keys.txt"
+REPO_URL = "https://raw.githubusercontent.com/ayobanw/keys.txt/main.lua/keys.txt"
